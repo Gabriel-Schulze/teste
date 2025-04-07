7f97c3a729590ee20b506b2e7e8e846e675234bd
Gabriel-Schulze <gabriel.eduardoschulze20@gmail.com>
2025-04-07 19:40:59 -0300

    Att menu

--- menu.php
+++ menu.php
@@ -1,22 +1,43 @@
-<?php 
-    session_start();
+<?php
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu</title>
+    <link rel="stylesheet" href="dist/css/bootstrap.min.css">
 </head>
+
 <body>
-    <h5>
-        <?php if($_SESSION['tipo_usuario'] == 1) {?>
-        <a href="usuarios.php">Usuários</a>
-        |
-        <?php }?>
-        <a href="produtos.php">Produtos</a>
-        |
-        <a href="index.php">Sair</a>
-    </h5>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="menu.php">Menu</a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <?php if ($_SESSION['tipo_usuario'] == 1) { ?>
+                    <li class="nav-item">
+                        <a href="usuarios.php" class="nav-link">Usuários</a>
+                    </li>
+                <?php } ?>
+                <li class="nav-item">
+                    <a href="produtos.php" class="nav-link">Produtos</a>
+                </li>
+                <li class="nav-item">
+                    <a href="index.php" class="nav-link">Sair</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+
+
+
+
+
+    <script src="dist/js/bootstrap.min.js"></script>
 </body>
+
 </html>
--- teste.php
+++ teste.php
@@ -1,31 +1,41 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
-<head>
-	<title>PHP</title>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" href="dist/css/bootstrap.min.css">
-</head>
-
-<body>
-	<form>
-		<div class="form-group">
-			<label for="exampleInputEmail1">Endereço de email</label>
-			<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Seu email">
-			<small id="emailHelp" class="form-text text-muted">Nunca vamos compartilhar seu email, com ninguém.</small>
+	
+	<head>
+		<title>PHP</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="dist/css/bootstrap.min.css">
+	</head>
+	
+	<body>
+		<div class="row">
+			<div class="col-5 form-group">
+				<label for="iNome">Nome:</label>
+				<input type="text" id="iNome" class="form-control">
+			</div>
+			<div class="col-4 form-group">
+				<label for="iEmail">Email:</label>
+				<input type="text" id="iEmail" class="form-control">
+			</div>
+			<div class="col-3 form-group">
+				<label for="iDtNasc">Data de Nascimento:</label>
+				<input type="date" id="iDtNasc" class="form-control">
+			</div>
 		</div>
-		<div class="form-group">
-			<label for="exampleInputPassword1">Senha</label>
-			<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
-		</div>
-		<div class="form-group form-check">
-			<input type="checkbox" class="form-check-input" id="exampleCheck1">
-			<label class="form-check-label" for="exampleCheck1">Clique em mim</label>
-		</div>
-		<button type="submit" class="btn btn-primary">Enviar</button>
-	</form>
-
-	<script src="dist/js/bootstrap.min.js"></script>
-</body>
-
-</html>
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		<script src="dist/js/bootstrap.min.js"></script>
+		
+	</body>
+	
+	</html>
